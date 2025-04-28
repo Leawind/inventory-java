dd6753f290c024e94f8390ae0ea080882c28ab54
leawind <leawind@yeah.net>
2025-04-28 12:13:22 +0800

    init gradle

---
+++ lib/build.gradle.kts
@@ -0,0 +1,82 @@
+plugins {
+    `java-library`
+    `maven-publish`
+}
+
+repositories {
+    maven {
+        name = "deno maven proxy"
+        url = uri("http://localhost:8080")
+        isAllowInsecureProtocol = true
+    }
+
+    mavenCentral()
+}
+
+dependencies {
+    testImplementation(libs.junit.jupiter)
+    testRuntimeOnly(libs.junit.platform.launcher)
+
+    api(libs.commons.math3)
+    implementation(libs.guava)
+}
+
+java {
+    toolchain {
+        languageVersion = JavaLanguageVersion.of(project.properties["org.gradle.java.version"] as String)
+    }
+
+    withSourcesJar()
+    withJavadocJar()
+}
+
+tasks.withType<JavaCompile> {
+    options.encoding = "UTF-8"
+}
+
+tasks.withType<Javadoc> {
+    options.locale = "en_US"
+    options.encoding = "UTF-8"
+    options {
+        this as StandardJavadocDocletOptions
+        // Suppress missing doclint warnings
+        addStringOption("Xdoclint:-missing", "-quiet")
+    }
+}
+
+tasks.named<Test>("test") {
+    useJUnitPlatform()
+}
+
+group = project.properties["lib_group_id"] as String
+version = project.properties["lib_version"] as String
+
+publishing {
+    publications {
+        create<MavenPublication>("maven") {
+            artifactId = project.properties["lib_artifact_id"] as String
+            group = project.properties["lib_group_id"] as String
+            version = project.properties["lib_version"] as String
+
+            from(components["java"])
+
+            pom {
+                url = "https://github.com/Leawind/inventory-java"
+
+                licenses {
+                    license {
+                        name = "MIT"
+                        url = "https://opensource.org/license/mit"
+                    }
+                }
+
+                developers {
+                    developer {
+                        id = "Leawind"
+                        email = "leawind@yeah.net"
+                    }
+                }
+            }
+        }
+    }
+}

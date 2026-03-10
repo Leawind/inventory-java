265a61f68ab0cf65e76d32085768a3ea3b3b8f6b
Leawind <leawind@yeah.net>
2026-03-10 11:23:25 +0800

    test(just): add tests for Just utility class

--- lib/src/test/java/io/github/leawind/inventory/just/JustTest.java
+++ lib/src/test/java/io/github/leawind/inventory/just/JustTest.java
@@ -1,3 +1,31 @@
 package io.github.leawind.inventory.just;
 
-public class JustTest {}
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+import org.junit.jupiter.api.Test;
+
+public class JustTest {
+
+  Result<Byte, String> add(byte a, byte b) {
+    int s = (int) a + (int) b;
+    if (s < Byte.MIN_VALUE || s > Byte.MAX_VALUE) {
+      return Result.err("overflow");
+    }
+    return Result.ok((byte) s);
+  }
+
+  void toPanic() {
+    throw JustError.panic("Panic!!");
+  }
+
+  @Test
+  void examples() {
+    var a = (byte) 127;
+    var b = (byte) 1;
+    var c = add(a, b);
+    assertEquals(Result.err("overflow"), c);
+
+    assertThrows(JustError.class, this::toPanic);
+  }
+}
